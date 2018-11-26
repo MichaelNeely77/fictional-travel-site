50cddac2ecff475b370de515ff72c54e49f8d260
Michael Neely <michael@michaelneely.net>
2018-11-25 21:43:02 -0500

    Completed the lesson on webpack and babel installation and ES6.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,19 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+import Person from './modules/Person';
 
-alert('A, b, c, 1, 2, 3.');
+class Adult extends Person {
+    payTaxes() {
+        console.log(this.name + ' owes $50K in taxes.');
+    }
+}
+
+alert('ABC 321');
 
 var john = new Person('John Doe', 'blue');
 john.greet();
 
-var jane = new Person('Jane Smith', 'green');
+var jane = new Adult('Jane Smith', 'orange');
 jane.greet();
+jane.payTaxes();
 
 $('h1').remove();
