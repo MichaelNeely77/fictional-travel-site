8ea688e74954ac87ca13bef8d5f7edda3c454007
Michael Neely <michael@michaelneely.net>
2018-12-08 00:47:24 -0500

    Adding the App.js changes that got caught in VS Code

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,17 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
-<<<<<<< HEAD
 import StickyHeader from './modules/StickyHeader';
-=======
->>>>>>> reveal-on-scroll
 
 var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($('.feature-item'), '85%');
-<<<<<<< HEAD
 new RevealOnScroll($('.testimonial'), '60%');
 var stickyHeader = new StickyHeader();
-=======
 new RevealOnScroll($('.testimonial'), '60%');
->>>>>>> reveal-on-scroll
