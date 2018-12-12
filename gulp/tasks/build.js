a849230f12a96ca91de96c761b2d450b101cf485
Michael Neely <michael@michaelneely.net>
2018-12-11 20:11:28 -0500

    Last commit for project...hopefully. Correcting build.js file.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,15 +1,9 @@
 var gulp = require('gulp'),
-<<<<<<< HEAD
-imagemin = require('gulp-imagemin');
-
-gulp.task('optimizeImages', function() {
-    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./apps/assets/images/icons/**/*'])
-=======
 imagemin= require('gulp-imagemin'),
 del = require('del'),
 usemin = require('gulp-usemin'),
 rev = require('gulp-rev'),
-cssnano = require('gulp-cssnano'),
+cssnano = require('gulp-cssnano'), 
 uglify = require('gulp-uglify'),
 browserSync = require('browser-sync').create();
 
@@ -39,37 +33,29 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
     
     return gulp.src(pathsToCopy)
         .pipe(gulp.dest('./docs'))
-})
+});
 
 gulp.task('optimizeImages', ['deleteDistFolder'], function() {
     return gulp.src(['./app/assets/images/**/*', '!./apps/assets/images/icons', '!./apps/assets/images/icons/**/*'])
->>>>>>> build-task
     .pipe(imagemin({
         progressive: true,
         interlaced: true,
         multipass: true
     }))
-<<<<<<< HEAD
-    .pipe(gulp.dest('./dist/assets/images'));
-});
-
-gulp.task('build', ['optimizeImages']);
-=======
-        .pipe(gulp.dest('./docs/assets/images'));
+    .pipe(gulp.dest('./docs/assets/images'));
 });
 
 gulp.task('useminTrigger', ['deleteDistFolder'], function() {
-    gulp.start('usemin');
-});
-
-gulp.task('usemin', ['styles', 'scripts'], function() {
+    gulp.start("usemin");
+  });
+  
+  gulp.task('usemin', ['styles', 'scripts'], function() {
     return gulp.src('./app/index.html')
-        .pipe(usemin({
-            css: [function() {return rev()}, function() {return cssnano()}],
-            js: [function() {return rev()}, function() {return uglify()}]
-        }))
-        .pipe(gulp.dest('./docs'));
-});
+      .pipe(usemin({
+        css: [function() {return rev()}, function() {return cssnano()}],
+        js: [function() {return rev()}, function() {return uglify()}]
+      }))
+      .pipe(gulp.dest('./docs'));
+  });
 
-gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
->>>>>>> build-task
+gulp.task('build', ['optimizeImages']);
